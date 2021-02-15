0a867525a4bb03381eeea55c7e61cb1678b48cab
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-02-15 10:05:10 +0000

    Initial Commit

---
+++ SuperBank/BankAccount.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBank 
+{
+    class BankAccount
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+
+        List<decimal> accountDeposits = new List<decimal>();
+        List<decimal> accountWithdrawals = new List<decimal>();
+
+        public void MakeDeposit()
+        {
+            Console.Write("\nEnter an amount you would like to deposit: £");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            if (depositAmount <= 0)
+            {
+                //throw new ArgumentOutOfRangeException(nameof(depositAmount), "Amount of deposit must be positive");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nDeposit amount must be positive");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Balance += depositAmount;
+                accountDeposits.Add(depositAmount);
+                Console.WriteLine($"Amount of deposits into the account {Number}: " + accountDeposits.Count);
+                Console.WriteLine("Account balance: £" + Balance);
+            }
+        }
+
+        public void MakeWithdrawal()
+        {
+            Console.Write("\nEnter an amount you would like to withdraw: £");
+            decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+            if (withdrawalAmount <= 0)
+            {
+                //throw new ArgumentOutOfRangeException(nameof(withdrawalAmount), "Amount of withdrawal must be positive");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nWithdrawal amount must be positive");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Balance - withdrawalAmount < 0)
+            {
+                //throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nNot sufficient funds for this withdrawal");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Balance -= withdrawalAmount;
+                accountWithdrawals.Add(withdrawalAmount);
+                Console.WriteLine($"Amount of withdrawals from the account {Number}: " + accountWithdrawals.Count);
+                Console.WriteLine("Account balance: £" + Balance);
+            }
+        }
+
+        public string GetAccountHistory()
+        {
+            var depositReport = new StringBuilder();
+            var withdrawalReport = new StringBuilder();
+            Console.WriteLine("\nAccount Transactions:");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("\nAccount Deposits");
+
+            //HEADER
+            depositReport.AppendLine("\nDate\t\tAmount");
+            foreach (var deposit in accountDeposits)
+            {
+                //ROWS
+                //Console.WriteLine($"£{deposit}");
+                depositReport.AppendLine($"{DateTime.Now.ToString("mm/dd/yy")}\t £{deposit}");
+            }
+            Console.WriteLine(depositReport.ToString());
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nAccount Withdrawals");
+
+            //HEADER
+            withdrawalReport.AppendLine("\nDate\t\tAmount");
+            foreach (var withdrawal in accountWithdrawals)
+            {
+                //ROWS
+                //Console.WriteLine($"£{withdrawal}");
+                withdrawalReport.AppendLine($"{DateTime.Now.ToString("mm/dd/yy")}\t £{withdrawal}");
+            }
+            Console.WriteLine(withdrawalReport.ToString());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nAccount Balance is: £" + Balance);
+            return null;
+        }
+    }
+}
+
---
+++ SuperBank/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+
+namespace SuperBank
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\tWelcome to the Super Bank!");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("\nTo get started please enter your full name: ");
+            account.Owner = Console.ReadLine();
+
+            Console.Write($"\nHi {account.Owner}, please enter your 10 digit account number: ");
+            account.Number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i > 0; i++)
+            {
+                if (account.Number.ToString().Length == 10)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nAccount Number must be 10 digits long");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\nPlease try again: ");
+                    account.Number = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine($"\nAccount {account.Number} was created for {account.Owner} with £{account.Balance}");
+
+            for (int i = 1; i > 0; i++)
+            {
+                Console.WriteLine("\nMain Menu");
+                Console.WriteLine("1. Make a deposit");
+                Console.WriteLine("2. Make a withdrawal");
+                Console.WriteLine("3. Retrieve account history");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your option (1-4): ");
+
+                int option = Int32.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        account.MakeDeposit();
+                        break;
+
+                    case 2:
+                        account.MakeWithdrawal();
+                        break;
+
+                    case 3:
+                        account.GetAccountHistory();
+                        break;
+
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("\nThank you for using the Super Bank :)");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Environment.Exit(-1);
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nPlease choose a valid option from the main menu");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+
+
+            // Test for a negative balance.
+            //try
+            //{
+            //    account.MakeWithdrawal();
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
+
